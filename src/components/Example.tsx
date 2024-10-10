2e222c55ff8364ca53b24e56065ee2a7df47ced6
Levente0324 <leventedukay@gmail.com>
2024-10-10 23:27:26 +0200

    added changes

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -5,10 +5,12 @@ const Example = () => {
   return (
     <div className="ml-20 mt-52 -rotate-3">
       <Post
+        id="pelda"
         title="This blog is cool"
         content="This blog app is soooo coool I love it!"
       />
       <Post
+        id="pelda1"
         title="My first post"
         content="This is my first ever post on this site : )"
       />
